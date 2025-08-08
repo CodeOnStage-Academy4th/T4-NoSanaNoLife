ff7af110e2d1a93942000d6eb96c4825567e1631
Hong Seokho <hongseokhodev@gmail.com>
2025-08-09 02:11:50 +0900

    Feat: Seat 모델, SeatPickView 레이아웃 구현

--- shittket/shittket/MainStream/SeatOptionView/SeatPickView.swift
+++ shittket/shittket/MainStream/SeatOptionView/SeatPickView.swift
@@ -7,12 +7,40 @@
 
 import SwiftUI
 
-struct SeatOptionView: View {
+struct SeatPickView: View {
+    let seats = [
+        Seat(id: 1, x: 100, y: 100, isSelected: true),
+        Seat(id: 2, x: 200, y: 100, isSelected: false),
+        Seat(id: 3, x: 300, y: 100, isSelected: false),
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image(.seats)
+                .resizable()
+                .scaledToFit()
+                .grayscale(1)
+            
+            ForEach(seats) { seat in
+                Button(action: {
+                }) {
+                    Rectangle()
+                        .foregroundStyle(seat.isSelected ? .blue : .red)
+                        .frame(width: 10, height: 10)
+                }
+                .position(x: seat.x, y: seat.y)
+            }
+        }
     }
 }
 
+struct Seat: Identifiable {
+    let id: Int
+    let x: CGFloat
+    let y: CGFloat
+    var isSelected: Bool
+}
+
 #Preview {
-    SeatOptionView()
+    SeatPickView()
 }
