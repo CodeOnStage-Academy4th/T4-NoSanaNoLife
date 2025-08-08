26eeef48161dff73e0adf7689cfa39a66ec35519
Hong Seokho <hongseokhodev@gmail.com>
2025-08-09 08:42:13 +0900

    Fix: 컨플릭트 수정

--- shittket/shittket/MainStream/PurchaseView/PurchaseView.swift
+++ shittket/shittket/MainStream/PurchaseView/PurchaseView.swift
@@ -11,22 +11,22 @@ struct PurchaseView: View {
     // 주문자 정보
     @State private var isNameCompleted: Bool = false
     @State private var isContactcompleted: Bool = true
-
+    
     // 결제 정보
     @State private var finalPrice: Int = 100
     @State private var isPriceCompleted: Bool = false
     @State private var bankMatched = false
-
+    
     // 결제 버튼 활성화 조건
     var isPaymentReady: Bool {
         isNameCompleted && isContactcompleted && isPriceCompleted
     }
-
+    
     @State private var toResult = false
-
+    
     // 알림창
     @State private var showKingAlert: Bool = false
-
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -35,24 +35,24 @@ struct PurchaseView: View {
                     VStack {
                         Spacer()
                             .frame(height: 20)
-
+                        
                         HStack {
                             Text("주문자 확인 및 예매 정보 및 결제 정보 및 결제 방식")
                                 .font(.headline)
                             Spacer()
                         }
                         Divider()
-
+                        
                         // 이름
                         NameInfoView(isNameCompleted: $isNameCompleted)
                             .padding(.vertical)
                         Divider()
-
+                        
                         // 전화번호
                         ContactInfoView(isContactcompleted: $isContactcompleted)
                             .padding(.vertical)
                         Divider()
-
+                        
                         // 농락요소1
                         HStack {
                             Button(action: {
@@ -71,14 +71,14 @@ struct PurchaseView: View {
                             HStack {
                                 Text("*")
                                     .foregroundColor(.red)
-
+                                
                                 Text("이 앱은 CRUD가 없기 때문에, 당신의 하나 뿐인 정보가 저장되지 않음을 알려드립니다.")
                                     .frame(width: 250)
                             }
                         }
                         .padding(.vertical)
                         Divider()
-                      
+                        
                         // 주문 상품
                         HStack {
                             Text("주문 상품")
@@ -96,12 +96,12 @@ struct PurchaseView: View {
                         // 가격
                         InfoRowView(label: "원래 가격", value: "18000원", isStrikethrough: true)
                         InfoRowView(label: "할인 쿠폰", value: "18000원 - \(18000 - finalPrice)원")
-
+                        
                         HStack {
                             Spacer()
                             Button(action: {
                                 // 쿠폰 어케 할거임
-
+                                
                             }) {
                                 HStack(spacing: 3) {
                                     Text("할인 쿠폰 적용")
@@ -115,9 +115,9 @@ struct PurchaseView: View {
                             .background(.green)
                             .clipShape(Capsule())
                         }
-
+                        
                         InfoRowView(label: "결제 금액", value: "\(finalPrice)원", valueColor: .green)
-
+                        
                         HStack {
                             Spacer()
                             Text("최소 결제 금액은 10,000원 입니다.")
@@ -127,23 +127,23 @@ struct PurchaseView: View {
                         }
                         .padding(.vertical)
                         Divider()
-
+                        
                         // 결제 수단
-                        //                        PaymentMethodView(bankMatched: $bankMatched)
-                        PaymentMethodView()
-
+                        PaymentMethodView(bankMatched: $bankMatched)
+                        //                        PaymentMethodView()
+                        
                     }
                     .padding(.horizontal)
-
+                    
                 }
-
+                
                 // MARK: - 결제하기 버튼
                 Button(action: {
                     if isPaymentReady {
                         toResult = true
                     }
                 }) {
-
+                    
                     Text("결제하기")
                         .font(.headline.bold())
                         .frame(maxWidth: .infinity)
@@ -156,14 +156,14 @@ struct PurchaseView: View {
                 .background(.black)
                 .disabled(!isPaymentReady)
                 .animation(.easeInOut, value: isPaymentReady)
-
+                
                 NavigationLink(
                     "",
                     destination: PurchaseReultView(),
                     isActive: $toResult
                 )
                 .hidden()
-
+                
             }
             .navigationTitle("결제 창")
             .navigationBarTitleDisplayMode(.inline)
@@ -171,7 +171,7 @@ struct PurchaseView: View {
                 if finalPrice > 10000 {
                     isPriceCompleted = true
                 }
-            }  
+            }
         }
     }
 }
