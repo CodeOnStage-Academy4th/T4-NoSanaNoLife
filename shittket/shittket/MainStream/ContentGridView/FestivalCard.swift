37b8b45fd98cc878da564d1d7ab541c9a3ade548
Hong Seokho <hongseokhodev@gmail.com>
2025-08-09 08:29:54 +0900

    Feat: 페스티벌카드 정보 수정

--- shittket/shittket/MainStream/ContentGridView/FestivalCard.swift
+++ shittket/shittket/MainStream/ContentGridView/FestivalCard.swift
@@ -17,7 +17,7 @@ struct FestivalCard: Identifiable {
 
 let festivalCards = [
     FestivalCard(imageReSource: .poster1, name: "Earth Rock\nExtravaganza", dateInfo: "07.26-28.2077", isAvailable: true),
-    FestivalCard(imageReSource: .poster2, name: "Earth Rock 2\nKorea", dateInfo: "07.26-28.2177", isAvailable: true),
+    FestivalCard(imageReSource: .poster2, name: "Earth Rock 2\nKorea", dateInfo: "07.26-28.2177", isAvailable: false),
     FestivalCard(imageReSource: .poster3, name: "Rock Festival", dateInfo: "Aug.18-20.2018", isAvailable: false),
     FestivalCard(imageReSource: .poster4, name: "INCHEON PENTAPORT", dateInfo: "Aug.9-11.2019", isAvailable: false),
     FestivalCard(imageReSource: .poster5, name: "2025 BUSAN\nInternational\nRock Festival", dateInfo: "09.26-28.2025", isAvailable: false),
